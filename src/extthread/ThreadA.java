5983ff8e9115478ab7a76d32f5e5045b88df87be
zhangzm201801 <zhangzm1117@126.com>
2019-06-22 22:21:39 +0900

    实现分批进行比赛

---
+++ src/extthread/ThreadA.java
@@ -0,0 +1,31 @@
+package extthread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class ThreadA extends Thread {
+
+	private CyclicBarrier cbRef;
+
+	public ThreadA(CyclicBarrier cbRef) {
+		super();
+		this.cbRef = cbRef;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println(Thread.currentThread().getName() + " begin ="
+					+ System.currentTimeMillis() + " 等待凑齐2个继续运行");
+			cbRef.await();
+			System.out.println(Thread.currentThread().getName() + "   end ="
+					+ System.currentTimeMillis() + " 已经凑齐2个继续运行");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
